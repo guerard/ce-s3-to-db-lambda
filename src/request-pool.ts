cff9507671fa28814b8fd23c40b023fc94db8e0b
Adrien Guerard <adrien.guerard@gmail.com>
2018-03-22 22:52:08 -0700

    .

--- src/request-pool.ts
+++ src/request-pool.ts
@@ -1,5 +1,3 @@
-import { noop } from 'lodash';
-
 export default class RequestPool {
   private currentIndex = 0;
   private pool: Promise<any>[] = [];
@@ -11,26 +9,13 @@ export default class RequestPool {
 
   public submit<O>(callable: () => Promise<O>): Promise<O> {
     return new Promise((resolve, reject) => {
-      this.selectPromise().then(() => {
-        const work = callable();
-        work.then(resolve, reject);
-        return work.then(noop, noop);
+      if ((this.currentIndex % this.poolSize) === 0) {
+        this.currentIndex = 0;
+      }
+      const next = this.currentIndex++;
+      this.pool[next] = this.pool[next].then(() => {
+        return callable().then(resolve, reject);
       });
     });
-  }
-
-  /**
-   * Override this to supply your own strategy for choosing which promise to attach
-   * the incoming one onto. Currently just does a dumb round-robin, which is fine for
-   * 99% of use-cases.
-   */
-  protected selectPromise() {
-    const promise = this.pool[this.currentIndex++ % this.poolSize];
-    if ((this.currentIndex % this.poolSize) === 0) {
-      // reset the counter; this ensures we don't increment beyond the range
-      // of valid integers in JS numbers (assuming poolSize is less than Number.MAX_SAFE_INTEGER)
-      this.currentIndex = 0;
-    }
-    return promise;
   }
 }
